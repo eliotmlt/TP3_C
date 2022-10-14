502dfd8c21b5d725903a275514983f8642d436dd
eliotmlt <eliot.malsert@gmail.com>
2022-10-14 13:41:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "tp3.c"
 #include "tp3.h"
 
 int main()
 {
-    matrice_creuse *Matrice = malloc(size(matrice_creuse)); //allocation memoire pour matrice
+    matrice_creuse *Matrice = malloc(sizeof(matrice_creuse)); //allocation memoire pour matrice
     matrice_creuse Mat = *Matrice;
     int N = 3;
     int M=3;
     Mat.Nlignes=N;
     Mat.Ncolonnes = M;
-    liste_ligne *ligne = malloc (N*size(liste_ligne)); //allocation memoire pour tableau de N lignes
-
+    Mat.tab_lignes = malloc(N*sizeof(liste_ligne));
+    //liste_ligne *ligne = malloc (N*sizeof(liste_ligne)); //allocation memoire pour tableau de N lignes
     remplirMatrice(Matrice, N, M);
     afficherMatrice(Mat);
+
+
+
     return 0;
 }
